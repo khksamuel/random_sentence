26d3c5d8ded00274565406e33ffd1e186ce7dd44
Ka Hei Samuel Kwok <a1821027@student.adelaide.edu.au>
2023-01-09 13:11:57 +1030

    translate attempt

--- random_sentence.py
+++ random_sentence.py
@@ -1,11 +1,15 @@
 import random
+# from translate import Translator
 
 def random_sentence(): # create random sentence based on the "articles, adjs, nouns, verbs, articles, adjs, nouns" format
     articles = open('articles.txt').read().splitlines()
     adjs = open('adjs.txt').read().splitlines()
     verbs = open('verbs.txt').read().splitlines()
     nouns  = open('nouns.txt').read().splitlines()
-    return ' '.join([random.choice(articles), random.choice(adjs), random.choice(nouns), random.choice(verbs), random.choice(articles), random.choice(adjs), random.choice(nouns)]).capitalize() + '.'
+    return str(' '.join([random.choice(articles), random.choice(adjs), random.choice(nouns), random.choice(verbs), random.choice(articles), random.choice(adjs), random.choice(nouns)]).capitalize() + '.')
 
 if __name__ == '__main__':
-    print(random_sentence())
+    sentence = random_sentence()
+    # translartor = Translator(to_lang="zh", from_lang="en")
+    # translated = translartor.translate((sentence))
+    print(sentence)
